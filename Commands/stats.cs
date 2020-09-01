2a88624ca1a2bd869ca4896bd0591eb6064ccfa3
BuildTools <unconfigured@null.spigotmc.org>
2020-09-01 21:35:37 +0100

    Fixed the Twitch Typo

--- Commands/stats.cs
+++ Commands/stats.cs
@@ -41,7 +41,7 @@ namespace CocaBot.Commands
             embed.AddField("SpookVooper",
                "Description: " + Data.description + "\nBalance: " + Data.credits + "\nDistrict: " + Data.district + "\nPost Likes: " + Data.post_likes + "\nComment Likes: " + Data.comment_likes + "\nAPI Use: " + Data.api_use_count);
             embed.AddField("Twitch",
-                "Twicth XP: " + Data.twitch_message_xp + "\nTwicth Messages: " + Data.twitch_messages);
+                "Twitch XP: " + Data.twitch_message_xp + "\nTwitch Messages: " + Data.twitch_messages);
             await ctx.RespondAsync(embed: embed).ConfigureAwait(false);
         }
 
@@ -76,7 +76,7 @@ namespace CocaBot.Commands
             embed.AddField("SpookVooper",
                "Description: " + Data.description + "\nBalance: " + Data.credits + "\nDistrict: " + Data.district + "\nPost Likes: " + Data.post_likes + "\nComment Likes: " + Data.comment_likes + "\nAPI Use: " + Data.api_use_count);
             embed.AddField("Twitch",
-                "Twicth XP: " + Data.twitch_message_xp + "\nTwicth Messages: " + Data.twitch_messages);
+                "Twitch XP: " + Data.twitch_message_xp + "\nTwitch Messages: " + Data.twitch_messages);
             await ctx.RespondAsync(embed: embed).ConfigureAwait(false);
         }
 
