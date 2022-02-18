6c3c335679b509d89d99485c5779ce586520f1cb
Coca <coca16622@gmail.com>
2022-02-18 19:19:36 +0000

    Making Valour ready for publishing

--- Valour/Commands.cs
+++ Valour/Commands.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Valour.Api.Items.Messages;
 using ValourSharp;
 using Valour.Api.Items.Planets.Members;
+using Shared;
 
 namespace Valour;
 
@@ -15,32 +16,43 @@ public static class Commands
     [Command("confirm", "conf")]
     public static async Task Confirm(PlanetMessage ctx)
     {
-        //do shit
+        CocaBotContext db = new();
+        var sender = await (await ctx.GetAuthorAsync()).GetUserAsync();
+
+        var user = db.Users.Where(x => x.ValourName == sender.Name).FirstOrDefault();
+        if (user is null) return;
+
+        user.Valour = sender.Id;
+        user.ValourName = null;
+        await db.SaveChangesAsync();
+
         await ctx.ReplyAsync("Linked Account!");
     }
 
-    [Command("call")]
-    public static async Task Call(PlanetMessage ctx, [Remainder] string call)
-    {
-        await ctx.ReplyAsync(call);
-    }
+    //for testing
 
-    [Command("call1")]
-    public static async Task Call1(PlanetMessage ctx, string call)
-    {
-        await ctx.ReplyAsync(call);
-    }
+    //[Command("call")]
+    //public static async Task Call(PlanetMessage ctx, [Remainder] string call)
+    //{
+    //    await ctx.ReplyAsync(call);
+    //}
 
-    [Command("decimal")]
-    public static async Task Decimal(PlanetMessage ctx, decimal number)
-    {
-        await ctx.ReplyAsync(number.ToString());
-    }
+    //[Command("call1")]
+    //public static async Task Call1(PlanetMessage ctx, string call)
+    //{
+    //    await ctx.ReplyAsync(call);
+    //}
 
-    [Command("member")]
-    public static async Task Member(PlanetMessage ctx, Api.Items.Users.User member, Api.Items.Users.User member2)
-    {
-        await ctx.ReplyAsync(member.Id + member.Name);
-        await ctx.ReplyAsync(member2.Id + member2.Name);
-    }
+    //[Command("decimal")]
+    //public static async Task Decimal(PlanetMessage ctx, decimal number)
+    //{
+    //    await ctx.ReplyAsync(number.ToString());
+    //}
+
+    //[Command("member")]
+    //public static async Task Member(PlanetMessage ctx, Api.Items.Users.User member, Api.Items.Users.User member2)
+    //{
+    //    await ctx.ReplyAsync(member.Id + member.Name);
+    //    await ctx.ReplyAsync(member2.Id + member2.Name);
+    //}
 }
--- Valour/Program.cs
+++ Valour/Program.cs
@@ -45,8 +45,8 @@ class Program
 
         await Start.Initialize(config.Email, config.BotPassword, prefixes.ToArray());
 
-        //OnMessageRecieved += async (message) =>
-        //    await MessageHandler(message);
+        OnMessageRecieved += async (message) =>
+            await MessageHandler(message);
 
         await Task.Delay(-1);
     }
@@ -54,35 +54,12 @@ class Program
     private static async Task MessageHandler(PlanetMessage ctx)
     {
         var sender = await (await ctx.GetAuthorAsync()).GetUserAsync();
-        if (!sender.Bot || Self != sender)
-        {
-            CocaBotContext db = new();
+        if (sender.Bot && Self == sender) return;
 
-            string id = await ValourToSVID(sender.Id, db);
+        CocaBotContext db = new();
+        string id = await ValourToSVID(sender.Id, db);
+        if (id is null) return;
 
-            await GetData($"https://ubi.vtech.cf/new_valour_message?svid={id}&valour_id={sender.Id}&key={UBIKey}");
-        }
-
-        //if ((ctx.Content.StartsWith('/') || ctx.Content.StartsWith("c/", StringComparison.InvariantCultureIgnoreCase) || ctx.Content.StartsWith(';'))
-        //    && !ctx.Content.Contains("conf", StringComparison.InvariantCultureIgnoreCase))
-        //{
-
-        //    var user = db.Users.Where(x => x.ValourName == sender.Name).FirstOrDefault();
-        //    if (user is null) return;
-        //    user.Valour = sender.Id;
-        //    user.ValourName = null;
-        //    await db.SaveChangesAsync();
-        //    await ctx.ReplyAsync("Linked Account!");
-        //}
-
-        //if (string.IsNullOrWhiteSpace(arg.Content)) return;
-
-        //var message = arg.Content.Trim();
-        //string commandprefix = "";
-        //commandprefix = prefix.SingleOrDefault(prefix => message[..prefix.Length] == prefix);
-
-        //if (commandprefix is null) return;
-
-        ////await ReplyMessage(arg, "");
+        await GetData($"https://ubi.vtech.cf/new_valour_message?svid={id}&valour_id={sender.Id}&key={UBIKey}");
     }
 }
